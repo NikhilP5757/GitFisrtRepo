58320db8187617566af9fd854406b1876942d94b
Nikhil Pawar <ngpawar@evolenthealth.com>
2020-06-19 16:22:02 +0530

    New practice file added

---
+++ Practice3.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 16:23:47 2020
+
+@author: NGPawar
+"""
+
+def first_fun():
+    print('hello')
+    return 
+
+first_fun()
+
+a = 'Nikhil'
+b = 'Pawar'
+c = a + '-' + b
+
+print("EVH training")
+
+print(c)
